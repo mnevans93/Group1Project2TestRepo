f11c2290bc07fcf1a3165f4c58f8c39d81433a3b
kennethpresedo <110072790+kennethpresedo@users.noreply.github.com>
2022-11-09 22:47:03 -0500

    Revert "merge "

--- whyNot.py
+++
@@ -1,8 +0,0 @@
-for fizzbuzz in range(101):
-    if fizzbuzz % 15 == 0:
-        print("fizzbuzz")
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
-    elif fizzbuzz % 5 == 0:
-        print("buzz")
-    print(fizzbuzz)
